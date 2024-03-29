a583a83e5830333629e843dd8f284c03e3c8201e
Jesse Jacobs <91500226+jjacobs-pmc@users.noreply.github.com>
2024-03-29 09:41:29 -0700

    Feature/wi 3927 (#3)
    * setup module
    
    * brackets
    
    * adds vars

---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "username" {
+    type = string
+    description = "value for username"
+    default = ""
+}
+variable "full_name" {
+    type = string
+    description = "value for full name"
+    default = ""
+}
+variable "locale" {
+  type = string
+  description = "value for locale"
+  default = ""
+}
+variable "role" {
+    type = string
+    description = "value for role"
+    default = ""
+}
+variable "timezone" {
+    type = string
+    description = "value for timezone"
+    default = ""
+}
+variable "tags" {
+    type = map
+    description = "map of tags"
+    default = {}
+}
+variable "user_address" {
+    type = map(string)
+    description = "map of address values"
+    default = ""
+}
+variable "country" {
+    type = string
+    description = "value for country"
+    default = ""
+}
+variable "state" {
+    type = string
+    description = "value for state"
+    default = ""
+}
+variable "city" {
+    type = string
+    description = "value for city"
+    default = ""
+}
+variable "line" {
+    type = string
+    description = "value for line"
+    default = ""
+}
+variable "zipcode" {
+    type = string
+    description = "value for zipcode"
+    default = ""
+}
+
+    
