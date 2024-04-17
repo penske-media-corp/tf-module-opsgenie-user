dcad33bb0a3d51813c545861f5f196c91a4c6324
Jesse Jacobs <91500226+jjacobs-pmc@users.noreply.github.com>
2024-04-17 09:01:24 -0700

    Feature/wi 3927 (#1)
    * setup module
    
    * setup module (#1)
    
    * brackets
    
    * Feature/wi 3927 (#2)
    
    * setup module
    
    * brackets
    
    * adds vars
    
    * Feature/wi 3927 (#3)
    
    * setup module
    
    * brackets
    
    * adds vars
    
    * updates vars
    
    * trying repo perms

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "opsgenie_user" "this" {
+  username          = "each.username"
+  full_name         = "each.full_name"
+  role              = "each.role"
+  locale            = "each.locale"
+  timezone          = "each.timezone"
+  tags              = ["each.tag"]
+  user_address {
+      country       = "each.country"
+      state         = "each.state"
+      city          = "each.city"
+      line          = "each.line"
+      zipcode       = "each.zipcode"
+  }
+}
