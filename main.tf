608da0bce44cb126a3f22440f872373cb211de19
Jesse Jacobs <91500226+jjacobs-pmc@users.noreply.github.com>
2024-05-14 13:10:07 -0700

    Feature/wi 3927 (#5)
    * setup module
    
    * setup module (#1)
    
    * brackets
    
    * Feature/wi 3927 (#2)
    
    * setup module
    
    * brackets
    
    * adds vars
    
    * Feature/wi 3927 (#3)
    
    * setup module
    
    * brackets
    
    * adds vars
    
    * updates vars
    
    * trying repo perms
    
    * rm uneeded vars
    
    * change resource name to use hyphen instead of underscore
    
    * adds lifecycle to username
    
    * changes each.values to use vars b/c they are already mapped in the root module
    
    * rm commented

--- main.tf
+++ main.tf
@@ -1,20 +1,15 @@
-resource "opsgenie-user" "this" {
-  username          = "each.username"
-  full_name         = "each.full_name"
-  role              = "each.role"
-  locale            = "each.locale"
-  timezone          = "each.timezone"
-  tags              = ["each.tag"]
+resource "opsgenie_user" "this" {
+  username          = var.username
+  full_name         = var.full_name
+  role              = var.role
+  locale            = var.locale
+  timezone          = var.timezone
+
   user_address {
-      country       = "each.country"
-      state         = "each.state"
-      city          = "each.city"
-      line          = "each.line"
-      zipcode       = "each.zipcode"
-  }
-  lifecycle {
-    ignore_changes = [
-      username
-     ]
+      country       = var.country
+      state         = var.state
+      city          = var.city
+      line          = var.line
+      zipcode       = var.zipcode
   }
 }
